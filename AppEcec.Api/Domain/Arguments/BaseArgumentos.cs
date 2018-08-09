b72630a942bb8a119d8d3bf2746ab81981d59b1e
Lucas Salvino de Deus <lucassalvino1@gmail.com>
2018-08-09 01:03:08 -0300

    Inicial API

---
+++ AppEcec.Api/Domain/Arguments/BaseArgumentos.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Domain.Arguments
+{
+    public class BaseArgumentos
+    {
+        public String Menssagem { get; set; }
+
+        public Boolean Error { get; set; }
+
+        public Guid IdSessao { get; set; }
+
+        public object Alertas { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ AppEcec.Api/Domain/Arguments/BaseFilters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain.Arguments
+{
+    public class BaseFilters
+    {
+        public Guid IdEntidade { get; set; }
+
+        public Guid IdSessao { get; set; }
+    }
+}
